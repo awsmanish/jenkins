99bf3033b5d2e7cdb71b0909a325aec562aa8c25
awsmanish <139372084+awsmanish@users.noreply.github.com>
2023-10-06 21:00:20 +0530

    Update onlinebook.groovy

--- onlinebook.groovy
+++ onlinebook.groovy
@@ -1,11 +1,11 @@
 pipeline{   // declarative pipeline
     agent {
-        label 'worker-node'
+        label 'worker-node2'
     }
     stages{
         stage('Git checkout from Github'){   // stage 1
             steps{
-                git branch: 'master', credentialsId: 'ec2', url: 'git@github.com:usertan786/onlinebookstore.git' 
+                git credentialsId: '4db30dcd-39cf-4f58-a1a6-44daf74ad2b5', url: 'git@github.com:awsmanish/onlinebookstore.git'   
                 sh 'ls'
             }
         }
@@ -22,12 +22,12 @@ pipeline{   // declarative pipeline
         }
         stage('push artifact to s3 bucket'){
             steps{
-                withAWS(credentials: 'new-creds', region: 'us-east-1') {
+                withAWS(credentials: 'news3', region: 'ap-southeast-2') {
                 sh '''  
                 sudo apt install awscli -y 
                 aws s3 ls 
-                sudo mv /home/ubuntu/workspace/onlinebook/target/onlinebookstore.war /home/ubuntu/workspace/onlinebook/target/bookstore-${BUILD_ID}.war
-                aws s3 cp /home/ubuntu/workspace/onlinebook/target/bookstore-${BUILD_ID}.war s3://student-new-jenkins
+                sudo mv /home/ubuntu/workspace/f1/target/onlinebookstore.war /home/ubuntu/workspace/f1/target/bookstore-${BUILD_ID}.war
+                aws s3 cp /home/ubuntu/workspace/f1/target/bookstore-${BUILD_ID}.war s3://student-war-file/
                 '''
                 }
             }
@@ -60,9 +60,9 @@ pipeline{   // declarative pipeline
         // }
         stage('deploy the artifact'){   
             steps{
-               withCredentials([sshUserPrivateKey(credentialsId: 'amazon', keyFileVariable: 'ec2', usernameVariable: 'ec2-user'), usernamePassword(credentialsId: 'creds-new', passwordVariable: 'AWS_SECRET_KEY', usernameVariable: 'AWS_ACCESS_KEY')]) {
+               withCredentials([sshUserPrivateKey(credentialsId: 'worker-node', keyFileVariable: 'ubuntu_user', usernameVariable: 'ubuntu'), usernamePassword(credentialsId: 'acess_or_ser', passwordVariable: 'AWS_SECRET_KEY', usernameVariable: 'AWS_ACCESS_KEY')]) {
                    sh '''  
-                   ssh -o StrictHostKeyChecking=no -i $ec2  ec2-user@54.166.174.172<<EOF
+                   ssh -o StrictHostKeyChecking=no -i $ubuntu_user  ubuntu@52.62.69.152<<EOF
                    sudo yum update -y
                    sudo yum install java-openjdk -y
                    curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz 
@@ -71,7 +71,7 @@ pipeline{   // declarative pipeline
                    aws configure set aws_access_key_id ${AWS_ACCESS_KEY} 
                    aws configure set aws_secret_access_key ${AWS_SECRET_KEY}
                    aws s3 ls 
-                   aws s3 cp s3://student-new-jenkins/bookstore-${BUILD_ID}.war .
+                   aws s3 cp s3://student-war-file/bookstore-${BUILD_ID}.war .
                    sudo mv bookstore-${BUILD_ID}.war bookstore.war
                    ls
                    sudo mv bookstore.war /opt/tomcat/webapps/
